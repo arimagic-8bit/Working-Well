9e31f7dabf4a88f2acb0116a1a8948682570c2a8
arimagic-8bit <baggerman1994@gmail.com>
2020-07-14 20:13:53 +0200

    Auth router complete. Create login, logout and another route to get current user info

--- routes/auth.js
+++ routes/auth.js
@@ -15,8 +15,8 @@ const {
   validationLogin
 } = require('./../helpers/middlewares');
 
-// POST - checks if user hasn't a cookie, if user info exist
-// and creates new user on DB
+// POST - checks if user hasn't a cookie, if user info exist,
+// creates new user on DB and creates user session
 
 authRouter.post('/signup', isNotLoggedIn, validationLogin, (req, res, next) => {
   const {
@@ -55,6 +55,56 @@ authRouter.post('/signup', isNotLoggedIn, validationLogin, (req, res, next) => {
       }
     })
     .catch((err) => next(createError(err)));
+})
+
+// POST - checks if user is not log in and if user exist
+// and creates user session
+
+authRouter.post('/login', isNotLoggedIn, validationLogin, (req, res, next) => {
+  const {
+    username,
+    password
+  } = req.body;
+
+  User.findOne({
+      username
+    })
+    .then((user) => {
+      if (!user) {
+        return next(createError(404));
+      } else {
+
+        // compares if given password === to user password
+        const passwordIsCorrect = bcrypt.compareSync(password, user.password);
+
+        if (passwordIsCorrect) {
+          user.password = "****";
+          req.session.currentUser = user;
+
+          res.status(200).json(user);
+        }
+      }
+    })
+    .catch(err => next(createError(err)));
+})
+
+// GET - checks if user is logged in and destroy session
+
+authRouter.get('/logout', isLoggedIn, (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(createError(err));
+    else {
+      res.status(204).send()
+    }
+  })
+})
+
+// GET - checks if user is logged in and gets user info
+
+authRouter.get('/me', isLoggedIn, (req, res, next) => {
+  const currentUserSession = req.session.currentUser;
+
+  res.status(200).json(currentUserSession);
 })
 
 module.exports = authRouter;

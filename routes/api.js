9d640766a62451d4c9a6af9c9f2c269e60705e9c
arimagic-8bit <baggerman1994@gmail.com>
2020-07-15 11:27:56 +0200

    Complete api routing. Create path to get activity info, edit activity info, remove activity and remove all activities

--- routes/api.js
+++ routes/api.js
@@ -25,4 +25,63 @@ apiRouter.post('/activity', function (req, res, next) {
     .catch(err => res.status(500).json(err));
 });
 
+// GET - gives activity info
+apiRouter.get('/activity', function (req, res, next) {
+  Activity.find()
+    .then((activities) => {
+      res.status(200).json(activities);
+    })
+    .catch(err => res.status(500).json(err))
+});
+
+// UPDATE - edit an activity info
+apiRouter.put('/activity/:id', (req, res, next) => {
+  const {
+    title,
+    completion,
+    rest
+  } = req.body;
+
+  const activityId = req.params.id;
+
+  Activity.findByIdAndUpdate({
+      _id: activityId
+    }, {
+      title,
+      completion,
+      rest
+    }, {
+      new: true
+    })
+    .then((updateActivity) => {
+      res.status(200).json(updateActivity)
+    })
+    .catch(err => res.status(500).json(err))
+});
+
+// DELETE - delete one activity from DB
+
+apiRouter.post('/activity/:id/delete', (req, res, next) => {
+  const activityId = req.params.id;
+
+  Activity.findByIdAndRemove({
+      _id: activityId
+    })
+    .then((removed) => {
+      res.status(200).json(removed);
+    })
+    .catch(err => res.status(500).json(err))
+});
+
+// DELETE - delete all activities from DB
+
+apiRouter.post('/activities/delete', (req, res, next) => {
+
+  Activity.remove({})
+    .then((activities) => {
+      res.status(200).json(activities);
+    })
+    .catch(err => res.status(500).json(err))
+});
+
 module.exports = apiRouter;
